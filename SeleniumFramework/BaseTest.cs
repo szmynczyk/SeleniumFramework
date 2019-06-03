6288cf673253775875fd95043ec2fce567a6702a
szmynczyk <marcin.szymczyk@interia.pl>
2019-06-03 21:03:56 +0200

    Brand new Selenium Framework

---
+++ SeleniumFramework/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFramework
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        IWebDriver Driver;
+
+        [Test]
+        public void GoToGoogleTest()
+        {
+            Driver = new ChromeDriver();
+            Driver.Url = @"https:\\google.com";
+            Driver.Quit();
+        }
+    }
+}
---
+++ SeleniumFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("850c6ba1-e59b-48b5-b8af-670a5176d92a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
